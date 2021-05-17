e2c254677ea6d72bb44cee10a13baa7c7f8a79db
gawronCode <gawroncode@gmail.com>
2021-05-17 21:59:44 +0200

    Implemented GetAll and Add - User controller

--- TAI_Projekt/Controllers/UsersController.cs
+++ TAI_Projekt/Controllers/UsersController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TAI_Projekt.Models.DbModels;
 using TAI_Projekt.Repositories.IRepos;
 
 
 namespace TAI_Projekt.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class UsersController : ControllerBase
     {
         private readonly IRepoUser _repoUser;
@@ -19,12 +23,21 @@ namespace TAI_Projekt.Controllers
             _repoUserRole = repoUserRole;
         }
 
-
-
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
         {
-            return "It's working - USERS";
+            var users = await _repoUser.GetAllAsync();
+            return Ok(users);
         }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Add(User user)
+        {
+            await _repoUser.CreateAsync(user);
+            return Ok();
+        }
+
+
     }
 }

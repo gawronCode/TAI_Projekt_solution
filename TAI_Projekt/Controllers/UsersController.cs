a91e4c58b61acc0983caed9b6d4f5c248c05d3f5
gawronCode <gawroncode@gmail.com>
2021-05-17 18:43:53 +0200

    Add project files.

---
+++ TAI_Projekt/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAI_Projekt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "It's working";
+        }
+    }
+}

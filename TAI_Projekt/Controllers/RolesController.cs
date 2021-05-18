8c227a8fbf58a658eafa2723bc07176f9ae082c9
gawronCode <gawroncode@gmail.com>
2021-05-18 13:27:09 +0200

    Implemented RolesController actions (Role CRUD)

--- TAI_Projekt/Controllers/RolesController.cs
+++ TAI_Projekt/Controllers/RolesController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TAI_Projekt.Models.DbModels;
 using TAI_Projekt.Repositories.IRepos;
 
 
 namespace TAI_Projekt.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class RolesController : ControllerBase
     {
         private readonly IRepoUser _repoUser;
@@ -19,11 +22,44 @@ namespace TAI_Projekt.Controllers
             _repoUserRole = repoUserRole;
         }
 
-
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
         {
-            return "It's working - ROLE";
+            var roles = await _repoRole.GetAllAsync();
+            return Ok(roles);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+            var role = await _repoRole.GetByIdAsync(id);
+            return Ok(role);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Role role)
+        {
+            await _repoRole.CreateAsync(role);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var role = await _repoRole.GetByIdAsync(id);
+            await _repoRole.DeleteAsync(role);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Update(Role role, int id)
+        {
+            var roleToUpdate = await _repoRole.GetByIdAsync(id);
+            roleToUpdate.Name = role.Name;
+            await _repoRole.UpdateAsync(roleToUpdate);
+            return Ok(roleToUpdate);
         }
     }
 }
